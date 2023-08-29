f416635b8f7148f886431699e77ef61c06e36d76
ABarskov <artyombarskov@gmail.com>
2023-08-29 16:36:51 +0300

    добавлен README для строителя, добавлен пример для обиночки на с++

---
+++ generative/singleton/c++/singleton.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Product {
+public:
+    static shared_ptr<Product> instance() {
+        static shared_ptr<Product> ptr(new Product());
+        return ptr;
+    }
+    void run() {cout << "Run method" << endl;}
+    Product(const Product&) = delete;
+    Product& operator=(const Product&) = delete;
+
+    ~Product() { cout << "Destructor" << endl; };
+private:
+    Product() {cout << "Constructor" << endl; }
+};
+
+int main() {
+    shared_ptr<Product> product(Product::instance());
+    product->run();
+    auto p = Product::instance();
+    p->run();
+}
